242dd78abcbc278b244d676fc282acf7d8c4c6a2
Muguku <machariam@kakbima.com>
2020-04-04 21:59:13 +0300

    move footer in own component

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  )
+}
