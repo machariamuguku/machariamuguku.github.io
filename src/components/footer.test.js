650ec9bd6f092faf83cb7655e6de4ce29f5e20e8
Muguku <hello@muguku.co.ke>
2020-04-25 18:18:08 +0300

    allign to RTL testing pilosophy, move tetst out of __tests__ folder

---
+++ src/components/footer.test.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Footer from "./footer"
+
+describe("<Footer />", () => {
+  let siteOwner = "The Test SiteOwner"
+  it("renders <Footer />", () => {
+    const { getByText } = render(<Footer siteOwner={siteOwner} />)
+    expect(
+      getByText(/All rights reserved, The Test SiteOwner,/i)
+    ).toBeInTheDocument()
+  })
+})
