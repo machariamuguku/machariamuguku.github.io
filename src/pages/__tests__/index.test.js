b08206cbee111430654c579b51863e666da7b218
Muguku <machariam@kakbima.com>
2020-04-25 18:21:19 +0300

    allign to RTL testing pilosophy

--- src/pages/__tests__/index.test.js
+++ src/pages/__tests__/index.test.js
@@ -1,18 +1,18 @@
 import React from "react"
-import { render } from "@testing-library/react"
-import { useStaticQuery } from "gatsby" // mocked
-
+import { render, waitFor } from "@testing-library/react"
+// already mocked in __mocks__
+import { useStaticQuery } from "gatsby"
 import Index from "../index"
 
 describe("<Index />", () => {
   beforeEach(() => {
-    // mock useStaticQuery
+    // mock useStaticQuery implementation
     useStaticQuery.mockImplementation(() => ({
       site: {
         siteMetadata: {
-          author: "Muguku",
           description: "My description",
-          title: "Muguku",
+          title: "Test Owner's Title",
+          owner: "The Test Owner",
         },
       },
     }))
@@ -21,35 +21,37 @@ describe("<Index />", () => {
   // Todo: do integration tests here.
   // if when you click on buttons things are mounted and unmounted.
   // e.g in the <HomeAboutContact /> section/component,
-  // when you click next/circles/nav menu what is rendered should change.
+  // when you click next/circles/nav menu what is rendered  change.
   // mock button clicks.
   // also maybe if you navigate anywhere the url changes.
   // and if it's anywhere other than `/#(sections)` 404 page is mounted
   // simulate all nav button clicks (about,contact,projects...)
 
-  it("should render <Header />", () => {
-    const { queryByTestId, debug } = render(<Index />)
-    // debug()
-    expect(queryByTestId("header-component")).toBeInTheDocument()
+  it("renders <Header />", () => {
+    const { getByText } = render(<Index />)
+    expect(getByText(/Test Owner's Title/i)).toBeInTheDocument()
   })
 
-  it("should render <HomeAboutContact />", () => {
-    const { queryByTestId } = render(<Index />)
-    expect(queryByTestId("home-about-contact-component")).toBeInTheDocument()
+  it("renders <HomeAboutContact />", () => {
+    const { getByText } = render(<Index />)
+    expect(getByText(/Hi there!/i)).toBeInTheDocument()
+    waitFor(() => expect(getByText(/I'm The Test Owner/i)).toBeInTheDocument())
   })
 
-  it("should render <Projects />", () => {
-    const { queryByTestId } = render(<Index />)
-    expect(queryByTestId("projects-component")).toBeInTheDocument()
+  it("renders <Projects />", () => {
+    const { getByText } = render(<Index />)
+    expect(getByText(/Popular Projects/i)).toBeInTheDocument()
   })
 
-  it("should render <Articles />", () => {
-    const { queryByTestId } = render(<Index />)
-    expect(queryByTestId("articles-component")).toBeInTheDocument()
+  it("renders <Articles />", () => {
+    const { getByText } = render(<Index />)
+    expect(getByText(/Popular Articles/i)).toBeInTheDocument()
   })
 
-  it("should render <Footer />", () => {
-    const { queryByTestId } = render(<Index />)
-    expect(queryByTestId("footer-component")).toBeInTheDocument()
+  it("renders <Footer />", () => {
+    const { getByText } = render(<Index />)
+    expect(
+      getByText(/All rights reserved, The Test Owner,/i)
+    ).toBeInTheDocument()
   })
 })
