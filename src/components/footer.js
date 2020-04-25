97d37440e6550907da383ec61a338728fbfe19b0
Muguku <hello@muguku.co.ke>
2020-04-26 01:41:10 +0300

    fix the footer to the bottom, add backlink to gaysby

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,31 @@
 import React from "react";
+import GatsbyLogo from "../images/Gatsby_Logo.svg";
+import styles from "./footer.module.css";
 
 export default function Footer({ siteOwner }) {
   return (
-    <footer>
-      All rights reserved, {siteOwner}, © {new Date().getFullYear()}, Built with
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <footer className={styles.footer}>
+      <div className={styles.footerParagraph}>
+        <span>
+          © {new Date().getFullYear()} {siteOwner}.{" "}
+        </span>
+        <span>All rights reserved. </span>
+        <span>Built with </span>
+        <a
+          className={styles.footerLinkColor}
+          href="https://www.gatsbyjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={GatsbyLogo}
+            alt="gatsby logo"
+            height="70px"
+            width="70px"
+            valign="middle"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
