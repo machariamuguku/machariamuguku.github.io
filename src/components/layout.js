49365d70ca52b4a58e214a203dbe25da8476b3c2
Muguku <machariam@kakbima.com>
2020-04-30 19:06:13 +0300

    move styling to stylesheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,15 +28,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <main className="children">{children}</main>
       <Footer siteOwner={data.site.siteMetadata.owner} />
     </>
   );
