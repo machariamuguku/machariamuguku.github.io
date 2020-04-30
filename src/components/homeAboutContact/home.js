cb71af69af94dc2e9ac2eef4f5346b4de6c5553d
Muguku <machariam@kakbima.com>
2020-04-30 23:58:09 +0300

    implement context for menu items

--- src/components/homeAboutContact/home.js
+++ src/components/homeAboutContact/home.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// context provider
+import { MenuContext } from "../menuContext";
+
+// components
 import SEO from "../seo";
-import styles from "./home.module.css";
 import { LeftCaret, RightCaret } from "./carets";
+
+// stylesheet
+import styles from "./home.module.css";
 // Todo: fn to type/delete I'm ....
 
+const navigationItems = ["Home", "About", "Contact"];
+
 export default function Home() {
+  const { activeMenu, setActiveMenu } = useContext(MenuContext);
+
+  const changeMenu = (direction) => {
+    const indexOfCurrentItem = navigationItems.indexOf(activeMenu);
+
+    // if direction is forward
+    if (direction === "Next") {
+      // set to index of next item
+      // if the next index is equal to the array length
+      // set to first index
+      if (indexOfCurrentItem + 1 === navigationItems.length) {
+        setActiveMenu(navigationItems[0]);
+      } else {
+        setActiveMenu(navigationItems[indexOfCurrentItem + 1]);
+      }
+    } else {
+      // set to index of previous item
+      // if the previous index is less than zero (first index)
+      // set to the biggest index
+      if (indexOfCurrentItem - 1 < 0) {
+        setActiveMenu(navigationItems[navigationItems.length - 1]);
+      } else {
+        setActiveMenu(navigationItems[indexOfCurrentItem - 1]);
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
       <SEO title="Home" />
@@ -16,9 +52,16 @@ export default function Home() {
             <span className={styles.brackets}>{"{"}</span>
           </div>
 
-          <div className={styles.item}>
+          <a
+            className={styles.item}
+            href="#Prev"
+            onClick={(e) => {
+              e.preventDefault();
+              changeMenu("Prev");
+            }}
+          >
             <LeftCaret />
-          </div>
+          </a>
         </div>
       </div>
 
@@ -48,9 +91,16 @@ export default function Home() {
       {/* right bracket */}
       <div className={`${styles.item} ${styles.bracketRight}`}>
         <div className={styles.container}>
-          <div className={styles.item}>
+          <a
+            className={styles.item}
+            href="#Next"
+            onClick={(e) => {
+              e.preventDefault();
+              changeMenu("Next");
+            }}
+          >
             <RightCaret />
-          </div>
+          </a>
           <div className={styles.item}>
             <span className={styles.brackets}>{"}"}</span>
           </div>
