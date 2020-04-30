b782facc35e1e90a166f70a2e29385967c0a3eef
Muguku <hello@muguku.co.ke>
2020-04-30 19:13:20 +0300

    add layout component to root page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 import Layout from "../components/layout";
-import SEO from "../components/seo";
+import { IntroAndSocialLinkLayout } from "../components/introAndSocialLinkLayout";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <IntroAndSocialLinkLayout />
   </Layout>
 );
 
