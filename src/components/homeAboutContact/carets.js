2dd4784a13a85c0b4379e66e2ec560aa7c510d05
Muguku <machariam@kakbima.com>
2020-04-30 19:08:22 +0300

    add navigation carets

---
+++ src/components/homeAboutContact/carets.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./carets.module.css";
+// react font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
+export function LeftCaret() {
+  return (
+    <FontAwesomeIcon icon={faCaretLeft} size="sm" className={styles.caret} />
+  );
+}
+
+export function RightCaret() {
+  return (
+    <FontAwesomeIcon icon={faCaretRight} size="sm" className={styles.caret} />
+  );
+}
