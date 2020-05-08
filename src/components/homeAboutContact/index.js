0525bcec35b1e95bc2f1c754eab05091bfd7271e
Muguku <machariam@kakbima.com>
2020-05-08 22:49:52 +0300

    add home, about and contact sections, use context

--- src/components/homeAboutContact/index.js
+++ src/components/homeAboutContact/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// context provider
+import { MenuContext } from "../menuContext";
+
+// stylesheet
 import styles from "./index.module.css";
+
 // components
 import Home from "./home";
+import { About } from "./about";
+import { Contact } from "./contact";
 import { NavigationCircles } from "./navigationCircles";
 
 export default function HomeAboutContact() {
+  const { activeMenu } = useContext(MenuContext);
   return (
     <div className={styles.container}>
-      <Home />
+      {activeMenu === "Home" && <Home />}
+      {activeMenu === "About" && <About />}
+      {activeMenu === "Contact" && <Contact />}
       <NavigationCircles />
     </div>
   );
