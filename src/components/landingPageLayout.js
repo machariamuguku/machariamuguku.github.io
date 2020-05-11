bbedfdae38208a71e3a48f97640bc6b1cd195bd6
Muguku <machariam@kakbima.com>
2020-05-11 22:28:51 +0300

    add chevron to next component

--- src/components/landingPageLayout.js
+++ src/components/landingPageLayout.js
@@ -4,12 +4,42 @@ import styles from "./landingPageLayout.module.css";
 import HomeAboutContact from "./homeAboutContact";
 import { SocialLinks } from "./socialLinks";
 
+// react font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 export const LandingPageLayout = () => {
   return (
-    <div className={styles.container}>
-      <HomeAboutContact />
-      <div className={styles.socialLinks}>
-        <SocialLinks />
+    <div className={styles.verticalContainer}>
+      <div className={styles.container}>
+        <HomeAboutContact />
+        <div className={styles.socialLinks}>
+          <SocialLinks />
+        </div>
+      </div>
+      <div
+        className={styles.caretDown}
+        onClick={() => {
+          console.log("go down implementation here");
+        }}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            console.log("go down implementation here");
+          }
+        }}
+        role="button"
+        tabIndex="0"
+      >
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          size="sm"
+          className={styles.goingDown}
+        />
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          size="sm"
+          className={styles.goingDown}
+        />
       </div>
     </div>
   );
