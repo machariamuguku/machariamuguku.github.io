959d7a7a37a9552e94c587c4f36ea20efe7fa453
Muguku <hello@muguku.co.ke>
2020-06-02 18:23:45 +0300

    css modules to styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
 
 import ArtDirectedFullBackground from "./ArtDirectedFullBackground";
 import Header from "./header";
 import Footer from "./footer";
-import "./layout.css";
+
+const Children = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,7 +38,7 @@ const Layout = ({ children }) => {
     <>
       <ArtDirectedFullBackground>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <main className="children">{children}</main>
+        <Children>{children}</Children>
       </ArtDirectedFullBackground>
       <Footer siteOwner={data.site.siteMetadata.owner} />
     </>
