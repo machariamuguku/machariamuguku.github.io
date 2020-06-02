e1d6472509ed1e1c1e6f521304c0eddae56d04f2
Muguku <hello@muguku.co.ke>
2020-06-02 18:23:11 +0300

    added global and theme styling providers

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,38 @@ import React from "react";
 // context provider
 import { MenuContextProvider } from "../components/menuContext";
 
+// global theme and theme provider
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
 // components
 import Layout from "../components/layout";
 import { LandingPageLayout } from "../components/landingPageLayout";
 
+const GlobalStyle = createGlobalStyle`
+html {
+  font-size: 100%;
+  font-family: ${(props) => props.theme.fontFamily};
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  background-color: #000000;
+}
+  body {
+    max-width: 100vw;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+    color: ${(props) => (props.whiteColor ? "white" : "#f5f5f5")};    
+  }
+`;
+
 const IndexPage = () => (
   <MenuContextProvider>
-    <Layout>
-      <LandingPageLayout />
-    </Layout>
+    <ThemeProvider theme={{ fontFamily: "sans-serif" }}>
+      <GlobalStyle whiteColor />
+      <Layout>
+        <LandingPageLayout />
+      </Layout>
+    </ThemeProvider>
   </MenuContextProvider>
 );
 
