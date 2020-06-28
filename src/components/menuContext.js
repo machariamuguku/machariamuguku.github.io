3e89c5088f3e1b5f41045db6bd646af3250554b5
Muguku <hello@muguku.co.ke>
2020-06-28 21:37:01 +0300

    seperate component update from menu update, usereducer from usestate

--- src/components/menuContext.js
+++ src/components/menuContext.js
@@ -1,11 +1,33 @@
-import React, { useState, createContext } from "react";
+import React, { useReducer, createContext } from "react";
 
 export const MenuContext = createContext();
 
 export function MenuContextProvider({ children }) {
-  const [activeMenu, setActiveMenu] = useState("Home");
+  const initialState = {
+    Menu: "Home",
+    Component: "Home"
+  };
+
+  function menuReducer(state, action) {
+    switch (action.type) {
+      case "CHANGE_MENU":
+        return { ...state, Menu: action.payload };
+      case "CHANGE_COMPONENT":
+        return { ...state, Component: action.payload };
+      default:
+        throw new Error("dispatched wrong action type");
+    }
+  }
+
+  const [activeMenuAndComponent, dispatchActiveMenuAndComponent] = useReducer(
+    menuReducer,
+    initialState
+  );
+
   return (
-    <MenuContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <MenuContext.Provider
+      value={{ activeMenuAndComponent, dispatchActiveMenuAndComponent }}
+    >
       {children}
     </MenuContext.Provider>
   );
