06f3bc4f67f594e7b7b5f07663bff0e9f9e2fc64
Muguku <hello@muguku.co.ke>
2020-07-02 12:57:40 +0300

    fix wrong initialization type

--- src/components/customHooks/useMediaQuery.js
+++ src/components/customHooks/useMediaQuery.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 export const useMediaQuery = (mediaQuery) => {
   // check if windows object is undefined to avoid build-time error (not available to nodejs runtime)
   const [matchesMediaQuery, setMatchesMediaQuery] = useState(() =>
-    typeof window !== `undefined` ? !!window.matchMedia(mediaQuery).matches : []
+    typeof window !== `undefined`
+      ? !!window.matchMedia(mediaQuery).matches
+      : false
   );
 
   useEffect(() => {
